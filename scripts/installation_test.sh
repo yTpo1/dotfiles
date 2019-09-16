ba83c28f33a52aff7aa25f5727ffac30abf6fd22
Nick <wildpoem111@gmail.com>
2019-09-16 11:02:40 +0100

    Update installation_test.sh

--- scripts/installation_test.sh
+++ scripts/installation_test.sh
@@ -57,33 +57,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # 3.2 Chroot
 echo " 3.2 Chroot"
 arch-chroot /mnt
-
-# 3.3 Time zone
-echo " 3.3 Time zone"
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-
-# 3.4 Localization
-echo " 3.4 Localization"
-cp ./files/locale.gen /etc/
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# 3.5 Network configuration
-echo " 3.5 Network configuration"
-# set up  a hostname
-echo "myarch" > /etc/hostname
-pacman -S networkmanager
-# enable it automaticaly through systemd
-systemctl enable NetworkManager
-
-# 3.6 Initramfs
-# 3.7 Root password
-echo " 3.7 Root password"
-echo "Enter root password: "
-passwd
-
-# 3.8 Boot loader
-echo "3.8 Boot loader"
-pacman -S grub
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
