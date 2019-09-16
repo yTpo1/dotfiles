470aaeaad52486e2ea78bab6962973bf2fb9780d
Nick <wildpoem111@gmail.com>
2019-09-16 11:49:11 +0100

    Update post_install_arch.sh

--- scripts/post_install_arch.sh
+++ scripts/post_install_arch.sh
@@ -3,19 +3,25 @@
 # Add a new user
 useradd -m -g wheel ghost1
 passwd ghost1
+# copy file in which group wheel is given sudo rights
+cp ./root/files/sudoers /etc/
 
-# GUI
-pacman -S xorg-server xorg-xinit
-pacman -S i3-gaps i3blocks rxvt-unicode dmenu
-
-# Fonts
-pacman -S ttf-linux-libertine ttf-inconsolata
-pacman -S noto-fonts
+# Install packages
+./root/package_installation.sh
 
 # System time - Network Time Protocol (NTP)
 pacman -S ntp
+# Enable it at boot
 systemctl enable ntpd.service
+# Start it immediately
 systemctl start ntpd.service
 
+
+# GUI
+#pacman -S xorg-server xorg-xinit
+#pacman -S i3-gaps i3blocks rxvt-unicode dmenu
+# Fonts
+#pacman -S ttf-linux-libertine ttf-inconsolata
+#pacman -S noto-fonts
 # Configure i3
-pacman -S feh
+#pacman -S feh
