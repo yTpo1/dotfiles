2cd63e8ffe9eb254efa05427e64b7ab7ebb984be
me <me@someemail.com>
2020-03-25 11:06:29 +0000

    add transmission

--- Scripts/ubuntu_pkg_install.sh
+++ Scripts/ubuntu_pkg_install.sh
@@ -78,3 +78,4 @@ sudo apt install manpages-posix-dev
 sudo apt install unrar
 sudo apt install mutt
 sudo apt install neomutt
+sudo apt install transmission-cli transmission-common transmission-daemon
