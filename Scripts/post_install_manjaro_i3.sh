1ae6742577b4879e383961e136d4e445408a06cf
Nick <me@someemail.com>
2021-08-06 17:07:12 +0100

    Edit manjaro post install script and add i3 config to symlinnk

--- Scripts/post_install_manjaro_i3.sh
+++ Scripts/post_install_manjaro_i3.sh
@@ -1,44 +1,78 @@
 #!/usr/bin/sh
 
-# Update system
-#sudo pacman -Syu
+printf "\nUpdate system\n\n"
+sudo pacman -Syu
 
-#sudo pacman -S firefox
+if ! [ -x $(command -v firefox) ]; then
+    sudo pacman -S firefox
+fi
 
-echo "dotfiles setup"
-sudo pacman -S stow
+printf "dotfiles setup\n\n"
+if ! [ -x $(command -v stow) ]; then
+    sudo pacman -S stow
+fi
+
 cd ~/Documents/dotfiles/manjaro-dotfiles/
 pwd
-
 if [ -e ~/.zshrc ]; then
     echo "Deleting zshrc"
     rm ~/.zshrc
 fi
-
 stow * -t ~/
 
-# Chanage shell to zsh
-sudo pacman -S zsh
-chsh -s $(which zsh)
+printf "Chanage shell to zsh\n\n"
+if ! [ -x $(command -v zsh) ]; then
+    sudo pacman -S zsh
+fi
+if [ $SHELL != "/bin/zsh" ]; then
+    echo Current shell not zsh, changing:; 
+    chsh -s $(which zsh)
+fi
 
-# Installing zsh plugin manager - Antibody
-curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
-# Installing zsh plugins
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+printf "Installing zsh plugin manager - Antibody\n\n"
+echo "Checking if antibody is installed"
+if ! [ -x $(command -v antibody) ]; then
+    echo Antibody is not installed, installing.
+    curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin 
+    printf "Installing zsh plugins\n\n"
+    antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+fi
 
-# Disable bios *beep* sounds on backspace
-sudo sh -c "echo 'blacklist pcspkr' > /etc/modprobe.d/nobeep.conf"
-#sudo echo blacklist pcspkr > /etc/modprobe.d/nobeep.conf
+if [ ! -e /etc/modprobe.d/nobeep.conf ]; then
+    printf "Disable bios *beep* sounds on backspace\n\n"
+    echo nobeep.conf does not exist, creating
+    sudo sh -c "echo 'blacklist pcspkr' > /etc/modprobe.d/nobeep.conf"
+fi
 
-#sudo pacman -S tmux
-#sudo pacman -S nvim
-#sudo pacman -S pulseaudio
-#sudo pacman -S pavucontrol
-#sudo pacman -S pass
-#sudo pacman -S 
-
+if ! [ -x $(command -v tmux) ]; then
+    sudo pacman -S tmux
+fi
+if ! [ -x $(command -v nvim) ]; then
+    sudo pacman -S nvim
+fi
+if ! [ -x $(command -v pulseaudio) ]; then
+    sudo pacman -S pulseaudio
+fi
+if ! [ -x $(command -v pavucontrol) ]; then
+    sudo pacman -S pavucontrol
+fi
+if ! [ -x $(command -v pass) ]; then
+    sudo pacman -S pass
+fi
+if ! [ -x $(command -v rofi) ]; then
+    sudo pacman -S rofi
+fi
+if ! [ -x $(command -v flameshot) ]; then
+    sudo pacman -S flameshot
+fi
+#if ! [ -x $(command -v ) ]; then
+    #sudo pacman -S 
+#fi
 
 
 #sudo pacman -S discord
 #sudo pacman -S telegram
+#sudo pacman -S dropbox
 #sudo pacman -S 
+
+#printf "\n \n\n"
