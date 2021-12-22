3ee31e7ca4d4b3bd0d2a928243c53ede793ffcbb
Nick <me@someemail.com>
2021-12-22 22:05:08 +0000

    Add clickable keyboard language switch to i3blocks menu

--- Manjaro-dotfiles/i3/.config/i3blocks/keyboard_layout.sh
+++ Manjaro-dotfiles/i3/.config/i3blocks/keyboard_layout.sh
@@ -10,4 +10,14 @@ case "$(xset -q|grep LED| awk '{ print $10 }')" in
   *) KBD="unknown" ;;
 esac
 
+case $BLOCK_BUTTON in
+	#1) langru && notify-send "keyswitch turned on";; # left click
+	1) setxkbmap -option grp:ctrl_alt_toggle us,ru && KBD="eng" ;;
+	#3) setxkbmap -option grp:ctrl_alt_toggle ru,us && KBD="rus" ;;
+	#2) notify-send "2j";; # 
+	#3) notify-send "3";; # right click
+	#4) notify-send "4";; # 
+	#5) notify-send "5";; #
+esac
+
 echo $KBD
